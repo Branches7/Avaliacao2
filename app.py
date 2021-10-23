6bc65111189e10518311400a802a6598c2ff30f8
Branches7 <50388052+Branches7@users.noreply.github.com>
2021-10-23 20:49:40 -0300

    finalização

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 def login():  # put application's code here
     return render_template('login.html')
 
-
-@app.route('/home', methods=['post'])
+@app.route('/home', methods=['post','get'])
 def home():
     bd = {'Dani': ['QQ', 'dan123'],
           'Thiago': ['QQ', 'th123']}
@@ -19,9 +18,11 @@ def home():
     validar = bd.get(user, ['erro', 'ex'])
 
     if validar[1] == senha:
-        listaJogos = ['FIFA 22', 'CS GO', 'CALL OF DUTY WARZONE', 'RAINBOW SIX', 'GTA V', 'BATTLEFIELD V', 'VALORANT', 'LEAGUE OF LEGENDS', 'DOTA 2', 'ROCKET LEAGUE']
+        listaJogos = ['FIFA 21', 'CS GO', 'CALL OF DUTY MODERN WARFARE', 'RAINBOW SIX', 'GTA V', 'BATTLEFIELD V', 'VALORANT',
+                      'LEAGUE OF LEGENDS', 'DOTA 2', 'ROCKET LEAGUE', 'FORZA 7', 'WATCH DOGS']
         jogos = {}
         cont = 1
+
         for jogo in listaJogos:
             img(jogo)
             jogos[cont] = [jogo, imagem[0], imagem[1]]
@@ -31,14 +32,15 @@ def home():
     else:
         return render_template('login.html', erro=True)
 
+
 def img(game):
     url = "https://bing-image-search1.p.rapidapi.com/images/search"
     global imagem
-    querystring = {"q": 'capa pequena jogo pc ' + game, "count": "2"}
+    querystring = {"q": 'logo do jogo ' + game, "count": "2"}
 
     headers = {
         'x-rapidapi-host': "bing-image-search1.p.rapidapi.com",
-        'x-rapidapi-key': "00e1cc2352mshd18db3cd042accdp135e2ejsnf272b01b1e3e"
+        'x-rapidapi-key': "ab9d73959fmsheb09c71ab215fe0p16199bjsndbe36a9cd935"
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
@@ -47,9 +49,12 @@ def img(game):
     imagem = value[0]['contentUrl'], value[1]['contentUrl']
 
 
-@app.route('/biblioteca')
+@app.route('/biblioteca', methods=['post','get'])
 def biblioteca():
-    return render_template('biblioteca.html')
+    game = request.form.get('nome')
+    imagem = request.form.get('img')
+    #TODO adicao de jogos
+    return render_template('biblioteca.html', game=game, imagem=imagem)
 
 
 if __name__ == '__main__':
